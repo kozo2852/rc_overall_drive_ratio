b259b7e303a59965e46e19e85a91ca8f84ad29e5
kozo2852 <skozokas@gmail.com>
2026-06-11 12:05:07 -0400

    Replace PDF tuning guide with formatted text parser

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3885850080" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2675878371" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
